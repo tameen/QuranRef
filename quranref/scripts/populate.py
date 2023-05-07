e4b35dde14a35dcfe02726ed9e3630bfa06b650d
tameen <tameenm@yahoo.com>
2023-05-08 02:30:56 +0500

    Update populate.py

--- quranref/scripts/populate.py
+++ quranref/scripts/populate.py
@@ -1,17 +1,17 @@
+
 import os
 import sys
 
 from pyramid.paster import get_appsettings, setup_logging
 
 from .. import load_project_settings, do_config
-
-
+"""
 def usage(argv):
-    cmd = os.path.basename(argv[0])
+    cmd = os.path.basename(sys.argv[2])
     print(('usage: %s <config_uri>\n'
            '(example: "%s development.ini")' % (cmd, cmd)))
     sys.exit(1)
-
+"""
 
 def _get_graph():
     from ..graph_models import gdb
@@ -26,20 +26,25 @@ def create_graph():
     gdb.create_graph(graph)
 
 
-def main(argv=sys.argv):  # pylint: disable=W0102
-
-    if len(argv) != 2:
+def populate_main(argv):  # pylint: disable=W0102
+    
+    """
+    if len(argv) != 17:
         usage(argv)
-
+    """
     load_project_settings()
 
-    config_uri = argv[1]
+    config_uri = argv
+    
     setup_logging(config_uri)
+   
     settings = get_appsettings(config_uri)
+    
     do_config({'__file__': config_uri}, **settings)
-
+    
     # create_graph()
     gdb, QuranGraph, _ = _get_graph()
     db_objects = [QuranGraph]
     gdb.create_all(db_objects)
+
 
